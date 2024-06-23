17193341f8b9dedf8cd231971c3df7b87ff1b569
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-23 16:26:51 +0200

    feat(server): add server solution

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,11 +1,19 @@
-import { RESTDataSource } from "@apollo/datasource-rest";
-import { TrackModel, AuthorModel } from "../models"
+import {RESTDataSource} from "@apollo/datasource-rest";
+import {TrackModel, AuthorModel, ModuleModel} from "../models"
 
 export class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
   getTracksForHome() {
     return this.get<TrackModel[]>("tracks");
+  }
+
+  getTrack(id: string) {
+    return this.get<TrackModel>(`track/${id}`)
+  }
+
+  getTrackModules(id: string) {
+    return this.get<ModuleModel[]>(`tracks/${id}/modules`);
   }
 
   getAuthor(authorId: string) {
--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,15 +1,21 @@
-import { Resolvers } from "./types";
+import {Resolvers} from "./types";
 
 export const resolvers: Resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
+    tracksForHome: (_, __, {dataSources}) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    track: (_, {id}, {dataSources}) => {
+      return dataSources.trackAPI.getTrack(id);
+    }
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
+    author: ({authorId}, _, {dataSources}) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+    modules: ({id}, _, {dataSources}) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    }
   },
 };
